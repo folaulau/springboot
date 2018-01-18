2d9bdcdb81cd1a8d4f76f5a0822a48a664679671
folaulau <folaukaveinga@gmail.com>
2018-01-18 11:44:43 -0800

    javascript-1.4
    added attributes and properties
    added modifying the document

--- springboot/src/main/resources/static/document.js
+++ springboot/src/main/resources/static/document.js
@@ -5,7 +5,7 @@ console.log("Hello Document!!!");
 // id must be unique
 // Only document.getElementById, not anyNode.getElementById
 let elem = document.getElementById("elem-content");
-console.log(elem.innerHTML);
+console.dir(elem);
 
 // getElementsByClassName
 // getElementsByName
@@ -35,4 +35,54 @@ console.log(chapter.closest('.book')); // UL
 console.log(chapter.closest('.contents')); // DIV
 
 console.log(chapter.closest('h1')); // null (because h1 is not an ancestor)
+
+// outerHTML
+let outer = document.getElementById("outer");
+console.log(outer.outerHTML);
+outer.outerHTML = "<p id='newElement'>A new element!</p>";
+let newElement = document.getElementById("newElement");
+console.log(newElement);
+
+// textContent
+let textContent = document.getElementById("news");
+console.log(textContent);
+console.log(textContent.textContent);
+
+// hidden
+let hidden = document.getElementById("hidden");
+hidden.hidden = false;
+
+// html attributes
+let customAttribute = document.getElementById("customAttribute");
+console.log("hasAttribute: "+customAttribute.hasAttribute("color"));
+console.log("getAttribute value: "+customAttribute.getAttribute("color"));
+
+// insertion method
+let div = document.createElement('div');
+div.className = "alert alert-success";
+div.innerHTML = "<strong>Hi there!</strong> You've read an important message.";
+
+document.body.appendChild(div);// append as last element on body
+
+console.log(document.body);
+
+
+// appendChild
+let newLi = document.createElement('li');
+newLi.innerHTML = 'Hello, world!';
+
+list.appendChild(newLi);
+
+list.insertBefore(newLi, list.children[1]);
+
+ol.before('before');
+ol.after('after');
+
+let prepend = document.createElement('li');
+prepend.innerHTML = 'prepend';
+ol.prepend(prepend);
+
+let append = document.createElement('li');
+append.innerHTML = 'append';
+ol.append(append);
 })();
