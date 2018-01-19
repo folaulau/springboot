a8dc16c8f3c8fa010569db49958b2e91634d1d8a
folaulau <folaukaveinga@gmail.com>
2018-01-18 16:32:36 -0800

    microservices-1.0
    clear all

--- springboot/src/main/java/com/folaukaveinga/springboot/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.folaukaveinga.springboot.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.folaukaveinga.springboot.domain.User;
-
-public interface UserRepository extends JpaRepository<User, Long>{
-
-}
