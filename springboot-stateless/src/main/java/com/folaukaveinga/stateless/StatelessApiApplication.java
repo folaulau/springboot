12abdaabe723fea575240f1cc44c5f89866c97f1
Folau Kaveinga <folaukaveinga@hotmail.com>
2018-02-26 23:26:44 -0800

    stateless-1.0
    added stateless setup

---
+++ springboot-stateless/src/main/java/com/folaukaveinga/stateless/StatelessApiApplication.java
@@ -0,0 +1,12 @@
+package com.folaukaveinga.stateless;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StatelessApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StatelessApiApplication.class, args);
+	}
+}
