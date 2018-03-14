6f04756f59f6fc699c056025d75b9553ced1960c
Folau Kaveinga <folaukaveinga@hotmail.com>
2018-03-14 00:29:01 -0700

    Start springboot testing
    - Add jpa configuration
    - Add User pojo
    - Add User unit test

--- springboot-jpa/src/main/resources/static/app.constants.js
+++
@@ -1,7 +0,0 @@
-(function () {
-    'use strict';
-    
-    angular
-        .module('springboot')
-        .constant('Status', {GOOD:"good",BAD:"bad",ERROR:"error",SUCCESS:"success"});
-})();
---
+++ springboot-testing/src/main/java/com/folaukaveinga/testing/repository/UserRepository.java
@@ -0,0 +1,25 @@
+package com.folaukaveinga.testing.repository;
+
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Future;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.util.concurrent.ListenableFuture;
+
+import com.folaukaveinga.testing.domain.User;
+import java.lang.String;
+import java.util.List;
+
+public interface UserRepository extends JpaRepository<User, Long>{
+
+	@Async
+	public Future<User> findByAge(int age);
+	
+	@Async
+	public CompletableFuture<User> findByName(String name);
+	
+	@Async
+	public ListenableFuture<User> findByEmail(String email);
+}
