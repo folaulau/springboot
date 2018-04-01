7ab87dd92d57c6ac3785418b144670a46eea085b
Folau Kaveinga <folaukaveinga@hotmail.com>
2018-04-01 10:25:24 -0700

    Add validator

--- springboot-validation/src/main/java/com/folaukaveinga/config/AppConfig.java
+++ springboot-validation/src/main/java/com/folaukaveinga/config/AppConfig.java
@@ -8,32 +8,17 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 @Configuration
 public class AppConfig {
-
-//	@Bean
-//	public MessageSource messageSource() {
-//	    ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
-//	    messageSource.addBasenames("classpath:messages");
-//	    return messageSource;
-//	}
-	
-	@Bean
-	public MessageSource messageSource() {
-	    ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
-	    messageSource.setBasename("message");
-	    messageSource.setDefaultEncoding("UTF-8");
-	    return messageSource;
-	}
-	 
-	@Bean
-	public LocaleResolver localeResolver() {
-	    SessionLocaleResolver localeResolver = new SessionLocaleResolver();
-	    localeResolver.setDefaultLocale(new Locale("es", "ES")); // change this
-	    
-	    return localeResolver;
-	}
+	 @Bean
+     public MessageSource messageSource () {
+         ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+         messageSource.setBasename("messages");
+         return messageSource;
+     }
 }
--- springboot-validation/src/main/java/com/folaukaveinga/validator/UserValidator.java
+++ springboot-validation/src/main/java/com/folaukaveinga/validator/UserValidator.java
@@ -4,6 +4,8 @@ import java.util.Locale;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Service;
 import org.springframework.validation.Errors;
 import org.springframework.validation.ValidationUtils;
@@ -15,6 +17,9 @@ import com.folaukaveinga.model.User;
 @Service
 public class UserValidator implements Validator {
 	private Logger log = LoggerFactory.getLogger(this.getClass());
+	
+	@Autowired
+	private MessageSource messageSource;
 
 	@Override
 	public boolean supports(Class<?> clazz) {
@@ -23,9 +28,13 @@ public class UserValidator implements Validator {
 
 	@Override
 	public void validate(Object obj, Errors errors) {
+		
 		log.info("validating User...\n");
 		//ValidationUtils.rejectIfEmpty(errors, "name", "user.name.empty");
-		ValidationUtils.rejectIfEmpty(errors, "email", "Email", "Email is not good");
+		
+		Locale spanish = new Locale("es");
+		
+		ValidationUtils.rejectIfEmpty(errors, "email", "Email", messageSource.getMessage("Email", null, spanish));
 		ValidationUtils.rejectIfEmpty(errors, "name", "NotBlank", "Not empty");
 		ValidationUtils.rejectIfEmpty(errors, "age", "Positive", "Not a postive number");
 		ValidationUtils.rejectIfEmpty(errors, "role", "NotBlank", "Not blank");
