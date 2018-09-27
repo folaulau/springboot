053bfa4f4a632aac4b22df351dc3db5182b6dba8
Folau <folaukaveinga@gmail.com>
2018-09-26 21:29:27 -0700

    setup

---
+++ springboot-activemq-clientb/src/main/java/com/kaveinga/SpringbootActivemqClientbApplication.java
@@ -0,0 +1,12 @@
+package com.kaveinga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootActivemqClientbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootActivemqClientbApplication.class, args);
+	}
+}
