c09519e40a044dc73ab1350176101783faf4d907
Folau <folaukaveinga@gmail.com>
2019-05-09 01:46:48 -0700

    added set up

--- springboot-dependency-injection/src/main/java/com/folaukaveinga/SpringbootDependencyInjectionApplication.java
+++ springboot-dependency-injection/src/main/java/com/folaukaveinga/SpringbootDependencyInjectionApplication.java
@@ -1,4 +1,4 @@
-package com.folaukaveinga.springbootdependencyinjection;
+package com.folaukaveinga;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
