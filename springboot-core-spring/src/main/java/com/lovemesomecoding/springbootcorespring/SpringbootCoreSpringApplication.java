4cd0d7ea152e6ee730aa21a5650d816ae428981e
Folau <folaukaveinga@gmail.com>
2019-09-09 16:36:53 -0700

    setup

---
+++ springboot-core-spring/src/main/java/com/lovemesomecoding/springbootcorespring/SpringbootCoreSpringApplication.java
@@ -0,0 +1,13 @@
+package com.lovemesomecoding.springbootcorespring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCoreSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootCoreSpringApplication.class, args);
+	}
+
+}
