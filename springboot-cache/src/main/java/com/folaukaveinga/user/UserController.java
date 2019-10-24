b35252724bc729d756f2f9975302b4d9e96e7906
folaulau <folaukaveinga@gmail.com>
2019-10-23 22:15:56 -0700

    added swagger

--- springboot-cache/src/main/java/com/folaukaveinga/user/UserController.java
+++ springboot-cache/src/main/java/com/folaukaveinga/user/UserController.java
@@ -12,6 +12,10 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
+@Api(value = "users", produces = "Rest API for user operations", tags = "User Controller")
 @RestController
 public class UserController {
 	private Logger log = LoggerFactory.getLogger(this.getClass());
@@ -19,18 +23,21 @@ public class UserController {
 	@Autowired
 	private UserService userService;
 	
+	@ApiOperation(value = "Get By Id")
 	@GetMapping("/users/{id}")
 	public ResponseEntity<User> get(@PathVariable("id") Long id){
 		log.info("getUser({})", id);
 		return new ResponseEntity<>(userService.getById(id), HttpStatus.OK);
 	}
 	
+	@ApiOperation(value = "Update")
 	@PutMapping("/users")
 	public ResponseEntity<User> update(@RequestBody User user){
 		log.info("update(..)");
 		return new ResponseEntity<>(userService.update(user), HttpStatus.OK);
 	}
 	
+	@ApiOperation(value = "Delete")
 	@DeleteMapping("/users")
 	public ResponseEntity<Boolean> delete(@RequestBody User user){
 		log.info("delete(..)");
