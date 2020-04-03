ac2f3e440d1251f5fbef6df866c11a9d4ec3833e
folaulau <folaukaveinga@gmail.com>
2020-04-03 17:52:39 -0600

    working with json string

--- springboot/src/main/java/com/folaukaveinga/springboot/rest/FileUploadRestController.java
+++ springboot/src/main/java/com/folaukaveinga/springboot/rest/FileUploadRestController.java
@@ -17,9 +17,11 @@ import org.springframework.web.multipart.MultipartFile;
 
 import com.folaukaveinga.springboot.domain.Attachment;
 import com.folaukaveinga.springboot.domain.JsonData;
+import com.folaukaveinga.springboot.domain.User;
 import com.folaukaveinga.springboot.service.AttachmentService;
 
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiParam;
 
 @Api(value = "files", produces = "Rest API for files operations", tags = "File Upload Controller")
 @RestController
@@ -71,13 +73,54 @@ public class FileUploadRestController {
 //	}
 
 	/**
-	 * Upload a single file with json data not working
+	 * Upload a single file with json data(only working with json file - json object
+	 * inside) <br>
+	 * 
 	 */
-	@RequestMapping(value = "/uploadWithData", method = RequestMethod.POST)
-	public ResponseEntity<Attachment> uploadSingleFileWithJsonObject(@RequestParam(value = "file") MultipartFile file,
-			@RequestParam JsonData jsonData) {
+	@RequestMapping(value = "/uploadWithJsonFileData", method = RequestMethod.POST, consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+	public ResponseEntity<Attachment> uploadSingleFileWithJsonFileObject(
+			@RequestPart(value = "file") MultipartFile file,
+			@ApiParam(name = "user") @RequestPart(name = "user") JsonData jsonData) {
 		log.info("uploading file to server. Filename: {}, File size: {}", file.getOriginalFilename(), file.getSize());
 		log.info("jsonData={}", jsonData.toJson());
 		return new ResponseEntity<>(attachmentService.saveFile(file), HttpStatus.OK);
 	}
+
+	/**
+	 * Upload a single file with json data(only working with json string oject) <br>
+	 * sample - {"id":9,"name":"Folau","age":32}
+	 */
+	@RequestMapping(value = {
+			"/uploadSingleFileWithJsonStringtifiedData" }, method = RequestMethod.POST, consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+	public ResponseEntity<Attachment> uploadSingleFileWithJsonStringtifiedObject(@RequestParam("user") String jsonUser,
+			@RequestParam("file") MultipartFile multipartFile) {
+		log.info("uploading attachment to server. File size: {}", multipartFile.getSize());
+		log.info("json user: " + User.fromJson(jsonUser).toString());
+		return new ResponseEntity<>(attachmentService.saveFile(multipartFile), HttpStatus.OK);
+	}
+
+	/**
+	 * Upload a single file with json data(only working with json string oject) <br>
+	 * sample - {"id":9,"name":"Folau","age":32}
+	 */
+	@RequestMapping(value = {
+			"/uploadMultipleFilesWithJsonStringtifiedData" }, method = RequestMethod.POST, consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+	public ResponseEntity<List<Attachment>> uploadMultipleFilesWithJsonStringtifiedObject(
+			@RequestParam("user") String jsonUser, @RequestParam("files") List<MultipartFile> multipartFiles) {
+		log.info("uploadMultipleFilesWithJsonStringtifiedObject. File size: {}", multipartFiles.size());
+		log.info("json user: " + User.fromJson(jsonUser).toString());
+		return new ResponseEntity<>(attachmentService.saveMultipartFiles(multipartFiles), HttpStatus.OK);
+	}
+
+//	/**
+//	 * Upload a single file with json data not working <br>
+//	 * 
+//	 */
+//	@RequestMapping(value = "/uploadWithDatad", method = RequestMethod.POST, consumes = {
+//			MediaType.APPLICATION_JSON_VALUE, MediaType.MULTIPART_FORM_DATA_VALUE })
+//
+//	public ResponseEntity<Attachment> uploadSingleFileWithJsonObject(@ModelAttribute User user) {
+//		log.info("age={}", user.getAge());
+//		return new ResponseEntity<>(attachmentService.saveFile(user.getFile()), HttpStatus.OK);
+//	}
 }
