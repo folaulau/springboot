09683d0a30a5a21271e51522160032d54e5b5bbb
Folau <folaukaveinga@gmail.com>
2020-04-11 11:10:41 -0600

    added all

--- springboot-swagger/src/main/java/com/folaukaveinga/swagger/config/SecurityConfig.java
+++ springboot-swagger/src/main/java/com/folaukaveinga/swagger/config/SecurityConfig.java
@@ -1,44 +1,38 @@
 package com.folaukaveinga.swagger.config;
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
 
-@Configuration
-@EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
-	
-	 private static final String[] AUTH_WHITELIST = {
-	            // -- swagger ui
-	            "/doc/v2/api-docs",
-	            "/doc/swagger-resources",
-	            "/doc/swagger-resources/**",
-	            "/doc/configuration/ui",
-	            "/doc/configuration/security",
-	            "/doc/swagger-ui.html",
-	            "/doc/webjars/**"
-	            // other public endpoints of your API may be appended to this array
-	    };
-	
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http
-	    .csrf().disable()
-	     .authorizeRequests()
-	      .antMatchers(AUTH_WHITELIST).permitAll()
-	      .anyRequest().authenticated()
-	      .and()
-	    .httpBasic().and()
-	    .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-	}
-	
-	@Override
-	public void configure(WebSecurity web) throws Exception {
-		web
-			.ignoring()
-			.antMatchers("/v2/api-docs", "/configuration/ui", "/swagger-resources", "/configuration/security", "/swagger-ui.html", "/webjars/**");
-	}
-}
+//@Configuration
+//@EnableWebSecurity
+//public class SecurityConfig extends WebSecurityConfigurerAdapter {
+//	
+//	 private static final String[] AUTH_WHITELIST = {
+//	            // -- swagger ui
+//	            "/doc/v2/api-docs",
+//	            "/doc/swagger-resources",
+//	            "/doc/swagger-resources/**",
+//	            "/doc/configuration/ui",
+//	            "/doc/configuration/security",
+//	            "/doc/swagger-ui.html",
+//	            "/doc/webjars/**"
+//	            // other public endpoints of your API may be appended to this array
+//	    };
+//	
+//	@Override
+//	protected void configure(HttpSecurity http) throws Exception {
+//		http
+//	    .csrf().disable()
+//	     .authorizeRequests()
+//	      .antMatchers(AUTH_WHITELIST).permitAll()
+//	      .anyRequest().authenticated()
+//	      .and()
+//	    .httpBasic().and()
+//	    .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+//	}
+//	
+//	@Override
+//	public void configure(WebSecurity web) throws Exception {
+//		web
+//			.ignoring()
+//			.antMatchers("/v2/api-docs", "/configuration/ui", "/swagger-resources", "/configuration/security", "/swagger-ui.html", "/webjars/**");
+//	}
+//}
--- springboot-swagger/src/main/java/com/folaukaveinga/swagger/config/WebConfig.java
+++ springboot-swagger/src/main/java/com/folaukaveinga/swagger/config/WebConfig.java
@@ -5,22 +5,22 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-@Configuration
-@EnableWebMvc
-public class WebConfig implements WebMvcConfigurer{
-	
-	@Override
-	public void addViewControllers(ViewControllerRegistry registry) {
-		registry.addRedirectViewController("/doc/v2/api-docs", "/v2/api-docs").setKeepQueryParams(true);
-		registry.addRedirectViewController("/doc/swagger-resources/configuration/ui","/swagger-resources/configuration/ui");
-		registry.addRedirectViewController("/doc/swagger-resources/configuration/security","/swagger-resources/configuration/security");
-		registry.addRedirectViewController("/doc/swagger-resources", "/swagger-resources");
-	}
-	
-	@Override
-	public void addResourceHandlers(ResourceHandlerRegistry registry) {
-		registry.addResourceHandler("/doc/**")
-		.addResourceLocations("classpath:/META-INF/resources/");
-	}
-}
+//
+//@Configuration
+//@EnableWebMvc
+//public class WebConfig implements WebMvcConfigurer{
+//	
+//	@Override
+//	public void addViewControllers(ViewControllerRegistry registry) {
+////		registry.addRedirectViewController("/doc/v2/api-docs", "/v2/api-docs").setKeepQueryParams(true);
+////		registry.addRedirectViewController("/doc/swagger-resources/configuration/ui","/swagger-resources/configuration/ui");
+////		registry.addRedirectViewController("/doc/swagger-resources/configuration/security","/swagger-resources/configuration/security");
+////		registry.addRedirectViewController("/doc/swagger-resources", "/swagger-resources");
+//	}
+//	
+//	@Override
+//	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+//		registry.addResourceHandler("/doc/**")
+//		.addResourceLocations("classpath:/META-INF/resources/");
+//	}
+//}
