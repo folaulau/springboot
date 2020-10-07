733b17b283e6cd52358eef4cbd5c1349df7ce058
folaulau <folaukaveinga@gmail.com>
2020-10-06 23:59:40 -0600

    add RetryTemplate

--- springboot-retry/src/main/java/com/lovemesomecoding/config/GlobalConfig.java
+++ springboot-retry/src/main/java/com/lovemesomecoding/config/GlobalConfig.java
@@ -1,11 +1,36 @@
 package com.lovemesomecoding.config;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.retry.annotation.EnableRetry;
+import org.springframework.retry.backoff.FixedBackOffPolicy;
+import org.springframework.retry.policy.SimpleRetryPolicy;
+import org.springframework.retry.support.RetryTemplate;
 
 @Configuration
 @EnableRetry
 public class GlobalConfig {
+
+    @Bean
+    public RetryTemplate retryTemplate() {
+        RetryTemplate retryTemplate = new RetryTemplate();
+
+        FixedBackOffPolicy fixedBackOffPolicy = new FixedBackOffPolicy();
+        fixedBackOffPolicy.setBackOffPeriod(3000l);
+
+        retryTemplate.setBackOffPolicy(fixedBackOffPolicy);
+
+        Map<Class<? extends Throwable>, Boolean> includeExceptions = new HashMap<>();
+        includeExceptions.put(RuntimeException.class, true);
+
+        SimpleRetryPolicy retryPolicy = new SimpleRetryPolicy(5, includeExceptions);
+        retryTemplate.setRetryPolicy(retryPolicy);
+
+        return retryTemplate;
+    }
 
 }
