46c02723f4b3b7ddd6143ed0553a28ee82a3e232
folaulau <folaukaveinga@gmail.com>
2020-10-07 10:43:33 -0600

    added multiplier to backoff

--- springboot-retry/src/main/java/com/lovemesomecoding/user/UserServiceImp.java
+++ springboot-retry/src/main/java/com/lovemesomecoding/user/UserServiceImp.java
@@ -1,5 +1,7 @@
 package com.lovemesomecoding.user;
 
+import java.time.LocalDateTime;
+
 import org.springframework.retry.annotation.Backoff;
 import org.springframework.retry.annotation.Recover;
 import org.springframework.retry.annotation.Retryable;
@@ -11,10 +13,10 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class UserServiceImp implements UserService {
 
-    @Retryable(value = {RuntimeException.class}, maxAttempts = 3, backoff = @Backoff(delay = 2000))
+    @Retryable(value = {RuntimeException.class}, maxAttempts = 5, backoff = @Backoff(delay = 2000, multiplier = 2))
     @Override
     public boolean sendEmail(User user) throws RuntimeException {
-        log.info("send email");
+        log.info("sending email at {}", LocalDateTime.now().toString());
         if (true) {
             throw new RuntimeException("error");
         }
