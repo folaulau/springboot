9d97fb861225d0cf9cc538787685c443378bf696
folaulau <folaukaveinga@gmail.com>
2021-03-24 10:52:16 -0600

    added rest assured bare

---
+++ springboot-with-rest-assured/src/main/java/com/lovemesomecoding/restassured/SpringbootWithRestAssuredApplication.java
@@ -0,0 +1,13 @@
+package com.lovemesomecoding.restassured;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootWithRestAssuredApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootWithRestAssuredApplication.class, args);
+	}
+
+}
---
+++ springboot-with-rest-assured/src/test/java/com/lovemesomecoding/restassured/SpringbootWithRestAssuredApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lovemesomecoding.restassured;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootWithRestAssuredApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
